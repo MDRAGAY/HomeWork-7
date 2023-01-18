730295ea472dd86c5b77a672f7c5c8b5e4bb1dbb
mdrag <m.dragay@yandex.ru>
2023-01-18 19:58:21 +0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,18 @@
 public class Main {
-        public static void main (String[] args) {
-            task1();
-
-        }
-        public static void task1() {
-            System.out.println("Задача 1");
-        }
+    public static void main(String[] args) {
+        task1();
+    }
+    public static void task1() {
+        System.out.println("Задача 1");
+        int savings = 15000;
+        int total = 0;
+        int i = 0;
+        while (total < 2_459_000) {
+            total = total + savings;
+            i++;
+            System.out.println("Месяц " + i + " сумма накоплений равна " + total + " рублей");}
+        System.out.println("Итого месяцев " + i + ", итого сумма равна " + total + " рублей");
+        { System.out.println();}
 
     }
+}
